10f08a5bf77b74a7a338617cda9d314fc293e3f7
Stefan Küchelmann <s.kuechelmann@outlook.com>
2025-04-24 22:50:13 +0200

    Save/Load works!

--- MeineFarm/Animal.cs
+++ MeineFarm/Animal.cs
@@ -1,34 +1,86 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
-/*
-if (weide == null ) 
-{
-    weide = Application.Current.MainWindow.FindName("Weide") as Canvas;
-}
-if(weide != null )
-{
 
-}*/
 namespace MeineFarm
 {
     internal class Animal
     {
+        private class AnimalData
+        {
+#pragma warning disable CS8618 
+            public string Name { get; set; }
+            public double Speed { get; set; }
+            public int Age { get; set; }
+            public string SoundFile { get; set; }
+            public Point Position { get; set; }
+            public string Type { get; set; } // "Dog", "Cat", etc.
+#pragma warning restore CS8618 
+        }
+
+        private class AnimalConverter : JsonConverter<Animal>
+        {
+            public override Animal? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            {
+                var animalData = JsonSerializer.Deserialize<AnimalData>(ref reader, options);
+                if (animalData == null) return null;
+                Canvas? Weide = Application.Current.MainWindow.FindName("Weide") as Canvas;
+                if(Weide == null)
+                {
+                    throw new NotSupportedException("No Canvas Weide in MainWindow");
+                }
+                Animal animal = animalData.Type switch
+                {
+                    "Dog" => new Dog(animalData.Name, animalData.Age, Weide, animalData.Position),
+                    "Cat" => new Cat(animalData.Name, animalData.Age, Weide, animalData.Position),
+                    _ => throw new NotSupportedException($"Unknown animal type: {animalData.Type}")
+                };
+
+                animal.Speed = animalData.Speed;
+                animal.SoundFile = animalData.SoundFile;
+                return animal;
+            }
+
+            public override void Write(Utf8JsonWriter writer, Animal animal, JsonSerializerOptions options)
+            {
+                var animalData = new AnimalData
+                {
+                    Name = animal.Name,
+                    Speed = animal.Speed,
+                    Age = animal.Age,
+                    SoundFile = animal.SoundFile,
+                    Position = animal.Position,
+                    Type = animal.GetType().Name
+                };
+
+                JsonSerializer.Serialize(writer, animalData, options);
+            }
+        }
+
+
+
+
+
         private class Speaking : Canvas
         {
             Image Bubble = new();
             Label Text = new();
             DispatcherTimer BubbleTimeoutTimer = new DispatcherTimer();
             Animal Outer;
-            public Speaking(Animal outer)
+            internal Speaking(Animal outer)
             {
                 Outer = outer;
                 Background = Brushes.LightBlue;
-                Bubble.Source = new BitmapImage(new("file://C:/Users/skuec/source/repos/MeineFarm/MeineFarm/Media/Sprechblase.png"));
+                Bubble.Source = new BitmapImage(outer.GetLoadUri("Sprechblase.png"));
                 Text.Content = Outer.Name;
                 Text.FontSize = 18;
                 Text.Foreground = Brushes.White;
@@ -43,6 +95,9 @@ namespace MeineFarm
                 BubbleTimeoutTimer.Interval = TimeSpan.FromSeconds(2);
                 BubbleTimeoutTimer.Start();
             }
+            // Interne Datenklasse für Serialisierung
+
+
             private void dispatcherTimer_Tick(object? sender, EventArgs e)
             {
                 Outer.Weide.Children.Remove(this);
@@ -55,32 +110,36 @@ namespace MeineFarm
         public int Age { get; set; }
         internal string SoundFile;
         internal Image AnimalImage = new();
-        internal Point Position = new(0, 0);
+        public Point Position { get; set; } = new(0, 0);
         internal Canvas Weide;
         private Vector moveDirection = new();
         private Point movePoint = new();
-
-        DispatcherTimer AniTimer = new();
-        public Point MovePoint 
+        private DispatcherTimer AniTimer = new();
+        internal bool selected = false;
+        internal bool Selected 
         { 
             get 
             { 
-                return movePoint; 
+                return selected; 
             } 
             set 
             { 
-                moveDirection = GetDirection(value); 
-                movePoint = value; 
+                selected = value; 
+                ShowSelectMarker(); 
             } 
-        } 
+        }
+        private Ellipse SelectIdentifier = new(); 
+        public event EventHandler? SelectedChanged;  //ADDTOBOOK
+
+
         public Animal(string name, int age, string soundfile, string graphfile, Canvas weide, Point position)
         {
             Name = name;
             Age = age;
             SoundFile = soundfile;
             Position = position;
-            AnimalImage.Source = new BitmapImage(new(graphfile));
-            Weide = weide;
+            AnimalImage.Source = new BitmapImage(GetLoadUri(graphfile));
+            if (Weide == null) Weide = weide;
             Weide.Children.Add(AnimalImage);
             Canvas.SetLeft(AnimalImage, Position.X);
             Canvas.SetTop(AnimalImage, Position.Y);
@@ -88,8 +147,53 @@ namespace MeineFarm
             AnimalImage.MouseDown += Klicked;
             AniTimer.Interval = TimeSpan.FromMilliseconds(15);
             AniTimer.Tick += AnimationStep;
-        }
 
+            SelectIdentifier.Stroke = Brushes.Red;
+            SelectIdentifier.StrokeThickness = 4;
+            SelectIdentifier.Width = 40;
+            SelectIdentifier.Height = 60;
+        }
+        public static void SaveFarm(List<Animal> FarmListe)
+        {
+
+            var options = new JsonSerializerOptions
+            {
+                Converters = { new Animal.AnimalConverter() }, // Zugriff auf den inneren Converter
+                WriteIndented = true
+            };
+            string json = JsonSerializer.Serialize(FarmListe, options);
+
+            File.WriteAllText("test.json", json);
+        }
+        public static List<Animal> LoadFarm()
+        {
+            var options = new JsonSerializerOptions
+            {
+                Converters = { new Animal.AnimalConverter() }, // Zugriff auf den inneren Converter
+                WriteIndented = true
+            };
+            // Deserialisieren
+            string json = File.ReadAllText("test.json");
+            List<Animal>? ReturnList = new List<Animal>();
+            ReturnList = JsonSerializer.Deserialize<List<Animal>>(json, options);
+            if(ReturnList == null) return new();    //Wenn Serializenicht 
+            return ReturnList;
+
+        }
+        ~Animal()
+        {
+            Weide.Children.Remove(SelectIdentifier);
+        }
+        private Uri GetLoadUri(string FileName)
+        {
+            string basePath = AppContext.BaseDirectory;
+            return new Uri($"file://{basePath}/Media/{FileName}"); //INTODABOOK
+        }
+        public void RemoveFromCanvas()
+        {
+            Weide.Children.Remove(AnimalImage);
+            if (Selected) Weide.Children.Remove(SelectIdentifier);
+        }
         private void AnimationStep(object? sender, EventArgs e)
         {
             Position += moveDirection;
@@ -97,39 +201,64 @@ namespace MeineFarm
                 Math.Abs(Position.Y - MovePoint.Y) < Speed)
             {
                 AniTimer.Stop();
+                Position = MovePoint;
                 moveDirection = new(0, 0);
             }
             Canvas.SetLeft(AnimalImage, Position.X);
             Canvas.SetTop(AnimalImage, Position.Y);
-        }
-
-        private void PlaceAt(Point? position = null)
-        {
-            if (position != null) Position = (Point)position;
-            Canvas.SetLeft(AnimalImage, Position.X);
-            Canvas.SetTop(AnimalImage, Position.Y);
+            if (Selected)
+            {
+                Canvas.SetLeft(SelectIdentifier, Position.X);
+                Canvas.SetTop(SelectIdentifier, Position.Y);
+            }
         }
         private Vector GetDirection(Point position)
         {
-            Vector direction = new Vector((position.X-Position.X), (position.Y-Position.Y));
+            Vector direction = new((position.X-Position.X), (position.Y-Position.Y));
             direction.Normalize();
             direction *= Speed;
             return direction;
         }
-        public void UpdatePosition()
-        {
-            AniTimer.Start();
-        }
-        public void Greet(object sender, MouseEventArgs e)
+        public void Greet(object sender, RoutedEventArgs e)
         {
             MediaPlayer AnimalSound = new();
-            AnimalSound.Open(new(SoundFile));
+            AnimalSound.Open(GetLoadUri(SoundFile));    //ADDTOBOOK
             AnimalSound.Play();
             Speaking s = new(this);
         }        
         public void Klicked(object sender, MouseEventArgs e)
         {
-            MessageBox.Show("Tier geklickt. Klick es auf die Klicker, damit es jault!!!","HrHrHr!!!");
+            Selected = !Selected;
+            if(SelectedChanged != null)
+                SelectedChanged.Invoke(this, e);
+        }
+
+        private void ShowSelectMarker()
+        {
+            if (Selected)
+            {
+                Weide.Children.Add(SelectIdentifier);
+                Canvas.SetLeft(SelectIdentifier, Position.X);
+                Canvas.SetTop(SelectIdentifier, Position.Y);
+            }
+            else
+            {
+                Weide.Children.Remove(SelectIdentifier);
+            }
+        }
+
+        public Point MovePoint
+        {
+            get
+            {
+                return movePoint;
+            }
+            set
+            {
+                moveDirection = GetDirection(value);
+                movePoint = value;
+                if (!AniTimer.IsEnabled) AniTimer.Start();
+            }
         }
         public static string[] GetDerivedClasses()
         {
@@ -149,9 +278,9 @@ namespace MeineFarm
     }
     class Dog : Animal
     {
-        public Dog(string name, int age, Canvas weide, Point position) : base(name, age, 
-            "file://C:/Users/skuec/source/repos/MeineFarm/MeineFarm/Media/wauwau.mp3",
-            "file://C:/Users/skuec/source/repos/MeineFarm/MeineFarm/Media/Hund.png", weide, position) 
+
+        public Dog(string name, int age, Canvas weide, Point position) : base(name, age,
+            "wauwau.mp3", "Hund.png", weide, position) 
         { 
             
         }
@@ -159,8 +288,7 @@ namespace MeineFarm
     class Cat : Animal
     {
         public Cat(string name, int age, Canvas weide, Point position) : base(name, age,
-    "file://C:/Users/skuec/source/repos/MeineFarm/MeineFarm/Media/meow.mp3",
-    "file://C:/Users/skuec/source/repos/MeineFarm/MeineFarm/Media/Katze.png", weide, position)
+            "meow.mp3", "Katze.png", weide, position)
         {
 
         }
--- MeineFarm/MainWindow.xaml.cs
+++ MeineFarm/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -9,7 +10,7 @@ namespace MeineFarm
 {
     public partial class MainWindow : Window
     {
-        AniImage AniCat;
+        //AniImage AniCat;
         Point NewPosition = new();
         List<Animal> animalList = new();
 
@@ -17,6 +18,7 @@ namespace MeineFarm
         public MainWindow()
         {
             InitializeComponent();
+            TierListe.SelectionMode = DataGridSelectionMode.Extended;//ADDTOBOOK
             TType.ItemsSource = Animal.GetDerivedClasses();
         }
 
@@ -41,37 +43,67 @@ namespace MeineFarm
                 MessageBox.Show("Klicke auf die Weide, um dem Tier ein Plätzchen zuzuweisen", "Du Honk!");
                 return;
             }
-            if(TType.SelectedItem.ToString() == "Cat") animalList.Add((Cat)new(TName.Text, age, Weide, NewPosition));
-            if(TType.SelectedItem.ToString() == "Dog") animalList.Add((Dog)new(TName.Text, age, Weide, NewPosition));
+            if (TType.SelectedItem.ToString() == "Cat")
+                animalList.Add(new Cat(TName.Text, age, Weide,
+                NewPosition));
+            if (TType.SelectedItem.ToString() == "Dog")
+                animalList.Add(new Dog(TName.Text, age, Weide,
+                NewPosition));
+            Animal NewAni = animalList[animalList.Count - 1];
+            NewAni.Speed = 1;
+            NewAni.SelectedChanged += AniSelect;
+
+            TierListe.ItemsSource = animalList;
             TierListe.ItemsSource = animalList;
             TierListe.Items.Refresh();
             Weide.Children.Remove(InsertFrame);
         }
 
+        private void AniSelect(object? sender, EventArgs e)
+        {
+            if(sender is Animal)
+            {
+                if (((Animal)sender).Selected)
+                    TierListe.SelectedItems.Add(sender);
+                else
+                    TierListe.SelectedItems.Remove(sender);
+            }
+        }
+
         private void Gruesse(object sender, RoutedEventArgs e)
         {
             if(TierListe.SelectedIndex > -1) 
-            animalList[TierListe.SelectedIndex].Greet(sender, e as MouseEventArgs);
+            animalList[TierListe.SelectedIndex].Greet(sender, e);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            List<ImageSource> KatzeAnimation = new List<ImageSource>();
-            List<String> AniFiles = Directory.GetFiles("C:\\Users\\skuec\\Desktop\\KatzeAnimated", "*.png").ToList();
-            foreach(String AniFile in AniFiles)
-            KatzeAnimation.Add(new BitmapImage(new(AniFile)));
-            AniCat = new(KatzeAnimation);
-            Weide.Children.Add(AniCat);
+            animalList = Animal.LoadFarm();
+            foreach(var animal in animalList)
+            {
+                animal.SelectedChanged += AniSelect;
+            }
+            TierListe.ItemsSource = animalList;
+            TierListe.Items.Refresh();
         }
 
-        private void Ani(object sender, RoutedEventArgs e)
+        private void DeleteAnimal(object sender, RoutedEventArgs e)
         {
-            foreach(Animal animal in animalList)
-            animal.UpdatePosition();
+            List<Animal> StayAlive = [.. animalList];
+            foreach(Animal AnimalToDelete in StayAlive) 
+            {
+                if(AnimalToDelete.Selected)
+                {
+                    AnimalToDelete.RemoveFromCanvas();
+                    animalList.Remove(AnimalToDelete);
+                }
+            }
+            TierListe.Items.Refresh();
         }
 
         private void Weide_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            if (e.OriginalSource != sender) return; //ADDTOBOOK
             NewPosition = e.GetPosition(this);
             InsertFrame.BorderBrush = Brushes.Green;
             InsertFrame.BorderThickness = new Thickness(2);
@@ -84,12 +116,22 @@ namespace MeineFarm
             Canvas.SetTop(InsertFrame, NewPosition.Y);
             if(TierListe.SelectedItem != null)
             {
-                Animal animal = animalList[TierListe.SelectedIndex];
-                animal.Speed = 10;
-                animal.MovePoint = NewPosition;
-                TierListe.ItemsSource = null;
-                TierListe.ItemsSource = animalList;
-                TierListe.UpdateLayout();
+                foreach(Animal ani in TierListe.SelectedItems)
+                {
+                    if(ani.Selected) 
+                    ani.MovePoint = NewPosition;
+                }
+            }
+        }
+
+        private void TierListe_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            foreach(Animal Animal in animalList) Animal.Selected = false;
+            foreach (Animal SelectedAnimal in TierListe.SelectedItems)
+            { 
+                if (!SelectedAnimal.Selected)
+                    SelectedAnimal.Selected = true; 
+
             }
         }
     }
